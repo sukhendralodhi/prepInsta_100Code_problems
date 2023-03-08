0769478000fb2504dd907a77bb3d51b1b82ce69e
sukhendra <mpct5jan1998@gmail.com>
2023-03-08 12:43:59 +0530

    v0.1

--- even_odd.cpp
+++ even_odd.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void evenOdd(int num) {
-  (num == 0 || num < 0) ? cout << "Please enter positive number"
-  : (num % 2 == 0)      ? cout << "Even number!"
-                        : cout << "Odd number!";
+void brute() {
+  int n;
+  cout << "Enter the number: ";
+  cin >> n;
+  if (n > 0) {
+    if (n % 2 == 0) {
+      cout << "Even number";
+    } else {
+      cout << "Odd number:";
+    }
+  } else {
+    cout << "Enter the positive number:";
+  }
+}
+
+void ternary() {
+  int n;
+  cout << "Enter the number: ";
+  cin >> n;
+  (n == 0 || n < 0) ? cout << "Please enter positive number"
+  : (n % 2 == 0)    ? cout << "Even number!"
+                    : cout << "Odd number!";
 }
 
 int main() {
-  int number;
-  cout << "Enter the number: ";
-  cin >> number;
-  evenOdd(number);
+  int exp;
+  cout << "Enter the value:" << endl
+       << "1 for Brute method" << endl
+       << "2 for ternary" << endl;
+  cin >> exp;
+
+  switch (exp) {
+  case 1:
+    cout << "You choose Brute:" << endl;
+    brute();
+    break;
+
+  case 2:
+    cout << "You chhose ternary:" << endl;
+    ternary();
+    break;
+
+  default:
+    cout << "Please chhose valid input: ";
+    break;
+  }
 }
